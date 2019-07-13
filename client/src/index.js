94677e863e62adc793492ee2b4970c89ed5d1b38
Blair Brownlie <bbrownli@asu.edu>
2019-07-13 10:46:01 -0700

    adding auth0 configuration file

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
+import { Auth0Provider } from "./react-auth0-wrapper";
+import config from "./auth_config.json";
+
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));

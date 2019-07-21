1754b6a9daa72e0dcbcce21a5c22896347652cfa
Michael Trepanier <mtrepanier91@gmail.com>
2019-07-21 16:58:59 -0700

    error found in postController.js while building unit tests corrected

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,13 +1,13 @@
 const db = require("../models");
 
 module.exports = {
-    findAllPosts = function (req, res) {
+    findAllPosts : function (req, res) {
         db.UserPosts
             .find(req.query)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err))
     },
-    create = function (req, res) {
+    create : function (req, res) {
         db.UserPosts
             .create(req.body)
             .then(dbModel => res.json(dbModel))

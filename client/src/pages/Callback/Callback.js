ff3d36ff76f1c806a0261fe18a5b283cf66aba0d
Blair Brownlie <bbrownli@asu.edu>
2019-07-28 19:25:58 -0700

    taking out a log that may have exposed sensitive data

--- client/src/pages/Callback/Callback.js
+++ client/src/pages/Callback/Callback.js
@@ -5,7 +5,6 @@ import auth0Client from '../../Auth/Auth';
 class Callback extends Component {
   async componentDidMount() {
     await auth0Client.handleAuthentication();
-    console.log(auth0Client.getProfile());
     this.props.history.replace('/');
   }
 
